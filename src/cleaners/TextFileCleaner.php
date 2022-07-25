1b88a677dce6e294f7a80692d1fd503a2d322bae
camilwojcik <camilwojcik@gmail.com>
2022-07-26 01:30:10 +0200

    stash of cleanerClass

---
+++ src/cleaners/TextFileCleaner.php
@@ -0,0 +1,22 @@
+<?php
+
+class TextFileCleaner implements Cleaner{
+
+    private String $path;
+    
+    public function __construct(String $path)
+    {
+        # code...
+    }
+    
+    public function clearAll(): void
+    {
+        # code...
+    }
+
+    public function clear(DateTime $dateTime): void
+    {
+        # code...
+    }
+
+}
